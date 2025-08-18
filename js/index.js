b23aafa794c696e2521b8c5f9ae369b98729d943
wing-0 <ookk8823@gmail.com>
2025-08-18 16:10:57 +0800

    dec

--- js/index.js
+++ js/index.js
@@ -1,6 +1,16 @@
-function myFunction() {
-    var element = document.body;
-    element.classList.toggle("dark-mode");
+let darkmode = localStorage.getItem('darkkmode')
+const themeSwitch = document.getElementById('theme-switch')
+const enableDarkmode = () => {
+    document.body.classList.add('darkmode')
+    localStorage.setItem('darkmode','active')
 }
-console.log(myFunction);
 
+const disableDarkmode = () => {
+    document.body.classList.remove('darkmode')
+    localStorage.setItem('darkmode',null)
+}
+
+if (darkmode === "active") enableDarkmode()
+themeSwitch.addEventListener("click", () => {
+    darkmode !=="active" ? enableDarkmode() : disableDarkmode()
+})
