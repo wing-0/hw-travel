9267021d0cb94200774ad15db2b8dda989f96470
wing-0 <ookk8823@gmail.com>
2025-08-22 16:15:22 +0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -7,7 +7,7 @@ document.addEventListener("DOMContentLoaded", () => {
     });
 });
 
-let darkmode = localStorage.getItem('darkkmode');
+let darkmode = localStorage.getItem('darkmode');
 const themeSwitch = document.getElementById('theme-switch');
 const enableDarkmode = () => {
     document.body.classList.add('darkmode');
