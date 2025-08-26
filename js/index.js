00ce975364fc38f5cf01fb7c9deaa5e0ad3bed9a
wing-0 <ookk8823@gmail.com>
2025-08-26 11:38:32 +0800

    ..?

--- js/index.js
+++ js/index.js
@@ -7,6 +7,17 @@ document.addEventListener("DOMContentLoaded", () => {
     });
 });
 
+//if (document.readyState !== 'loading') {
+    //console.log('document is already ready, just execute code here');
+    //myInitCode();
+//} else {
+    //document.addEventListener('DOMContentLoaded', function(){
+        //console.log('document was not ready, place code here');
+        //myInitCode();
+    //});
+//}
+function myInitCode() {}
+
 let darkmode = localStorage.getItem('darkmode');
 const themeSwitch = document.getElementById('theme-switch');
 const enableDarkmode = () => {
